7d51ef078ca0c1d101b7eb16ca0ba3abd0047f96
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2024-10-18 17:43:31 +0200

    Remove tournament info in mobile mode

--- src/app/(non-authed)/community-battles/components/tournament-mobile.tsx
+++ src/app/(non-authed)/community-battles/components/tournament-mobile.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from "react";
 import { ArrowClockwise } from "@phosphor-icons/react";
 
 import Spinner from "@/components/ui/spinner"; // Importing Spinner component
-import { jibril } from "@/utils/fonts";
 
 import useGetTournament from "../hooks/use-get-tournament";
 import CommunityLeaderboard from "./community-leaderboard";
@@ -67,67 +66,25 @@ const TournamentMobile = () => {
           className={`flex-1 p-2 ${activeTab === 1 ? "bg-red-400 text-black" : " text-white"}`}
           onClick={() => setActiveTab(1)}
         >
-          Tournament Info
-        </button>
-        <button
-          className={`flex-1 p-2 ${activeTab === 2 ? "bg-red-400 text-black" : " text-white"}`}
-          onClick={() => setActiveTab(2)}
-        >
           Communities
         </button>
 
         <button
-          className={`flex-1 p-2 ${activeTab === 3 ? "bg-red-400 text-black" : " text-white"}`}
-          onClick={() => setActiveTab(3)}
+          className={`flex-1 p-2 ${activeTab === 2 ? "bg-red-400 text-black" : " text-white"}`}
+          onClick={() => setActiveTab(2)}
         >
           Leaderboard
         </button>
       </div>
 
       {activeTab === 1 && (
-        <div className="flex flex-col items-center space-y-6 rounded-lg  p-6 shadow-lg">
-          <h1 className="text-4xl font-bold text-red-400" style={jibril.style}>
-            {name}
-          </h1>
-          <h2 className="text-2xl font-semibold text-gray-300">Season {season}</h2>
-          <p className="text-sm text-gray-400">
-            Starts: {formatDate(startDate)} <br />
-            Ends: {formatDate(endDate)}
-          </p>
-
-          {/* Prize Section */}
-          <div className="mt-6 w-full space-y-4 rounded-lg bg-gray-900 p-6 text-center shadow-md">
-            <h1 className="text-2xl font-semibold text-red-400" style={jibril.style}>
-              Ultimate Battle Prize
-            </h1>
-            <h1 className="text-5xl font-extrabold text-white drop-shadow-md">
-              {`${prize} ${prizeToken}`}
-            </h1>
-            <a
-              href="https://www.ora.io/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center space-x-2"
-            >
-              <h1 className="text-sm font-bold text-gray-300">Powered by ORA Protocol</h1>
-              <img
-                src="/images/logos/ora-logo.png"
-                alt="ORA Logo"
-                className="h-8 w-8 object-contain"
-              />
-            </a>
-          </div>
-        </div>
-      )}
-
-      {activeTab === 2 && (
-        <div className="flex flex-col items-center space-y-4">
+        <div className="mt-8 flex flex-col items-center space-y-4">
           <CommunityLeaderboard communities={communities} />
         </div>
       )}
 
-      {activeTab === 3 && (
-        <div>
+      {activeTab === 2 && (
+        <div className="mt-8">
           <CommunityTrack
             communities={communities}
             selectedCommunity={selectedCommunity}
